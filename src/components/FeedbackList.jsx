9e6d75dcc9325e3cc1d67a6a7270ed88ed7b559e
whitemagic <shivambohracse@gmail.com>
2023-01-08 22:13:29 +0530

    added mock backend json server

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -3,10 +3,12 @@ import { motion, AnimatePresence } from "framer-motion";
 import FeedbackItem from "./FeedbackItem";
 import { useContext } from "react";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList() {
-  const { feedback } = useContext(FeedbackContext);
-  if (!feedback || feedback.length === 0) return <p>No Feedback Yet</p>;
+  const { feedback, isLoading } = useContext(FeedbackContext);
+  if (!isLoading && (!feedback || feedback.length === 0))
+    return <p>No Feedback Yet</p>;
 
   // return (
   //   <div className="feedback-list">
@@ -16,15 +18,14 @@ function FeedbackList() {
   //   </div>
   // );
 
-  return (
+  return isLoading ? (
+    <Spinner />
+  ) : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) => (
           <motion.div key={item.id} layout>
-            <FeedbackItem
-              key={item.id}
-              item={item}
-            />
+            <FeedbackItem key={item.id} item={item} />
           </motion.div>
         ))}
       </AnimatePresence>
